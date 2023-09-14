f09661f121a0d3dbb023a6be3c1a7b116541c333
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2023-09-14 18:22:11 +0200

    Add files via upload

--- chart_generator.py
+++ chart_generator.py
@@ -26,7 +26,7 @@ def read_file():
             le.config(text="Proszę podać nazwę pliku")
             
     except FileNotFoundError:
-        le.config(text="Plik o tej nazwie nie istnieje")
+        le.config(text="Plik nie istnieje")
     
 def create_chart(dtype, filename):
     top = Toplevel()
@@ -38,47 +38,56 @@ def create_chart(dtype, filename):
     chtype = ["kolumnowy", "liniowy"]
     
     def configure_chart():
+        #przechowuje dane pobrane od użytkownika
+        chart_config = {
+            'data':'',
+            'type':'',
+            'labelx':'',
+            'labely':'',
+            'title':'',
+            'datInd':0,
+            'typeInd':0
+            }
+
         #wybór danych
-        select_data = data.get()
+        chart_config['data'] = data.get()
             
         #wybór typu wykresu
-        chart_type = chart.get()
+        chart_config['type'] = chart_type.get()
 
-        x = osX.get()
-        y = osY.get()
-        chtitle = title.get()
-        
-        if select_data not in dtype or chart_type not in chtype:
+        chart_config['labelx'] = osX.get()
+        chart_config['labely'] = osY.get()
+        chart_config['title'] = title.get()
+                
+        if chart_config['data'] not in dtype or chart_config['type'] not in chtype:
             laberr.config(text="Proszę wybrać opcje z list")
+        
         else:
-            datind = dtype.index(select_data)
-            graph = chtype.index(chart_type)
+            chart_config['datInd'] = dtype.index(data.get())
+            chart_config['typeInd'] = chtype.index(chart_type.get())
             laberr.config(text="")
-            draw_graph(datind, graph,chtitle, x,y)
+            draw_graph(chart_config)
                                                  
     data = ttk.Combobox(top, values=dtype, state="readonly")
     data.grid(row = 0, column = 0)
     data.set("Wybierz typ danych")
         
-    chart = ttk.Combobox(top, values=chtype, state="readonly")
-    chart.grid(row = 1, column = 0)
-    chart.set("Wybierz typ wykresu")
+    chart_type = ttk.Combobox(top, values=chtype, state="readonly")
+    chart_type.grid(row = 1, column = 0)
+    chart_type.set("Wybierz typ wykresu")
 
     #tytuł wykresu
-    ltitle=Label(top, text="Tytuł wykresu")
-    ltitle.grid(row=3,column=0)
+    ltitle=Label(top, text="Tytuł wykresu").grid(row=3,column=0)
     title= Entry(top,bd=3, width=25)
     title.grid(row=3,column=1)
 
     #oznaczenie osi X
-    loX=Label(top, text="Nazwa osi X")
-    loX.grid(row=4,column=0)
+    loX=Label(top, text="Nazwa osi X").grid(row=4,column=0)
     osX= Entry(top,bd=3, width=25)
     osX.grid(row=4,column=1)
     
     #oznaczenie osi Y
-    loY=Label(top, text="Nazwa osi Y")
-    loY.grid(row=5,column=0)
+    loY=Label(top, text="Nazwa osi Y").grid(row=5,column=0)
     osY= Entry(top,bd=3,width=25)
     osY.grid(row=5,column=1)
     
@@ -88,53 +97,47 @@ def create_chart(dtype, filename):
     laberr = Label(top, text = "")
     laberr.grid(row=6,column=0)
     
-    def draw_graph(dati,chai,title, osx,osy):
+    def draw_graph(chartOpt):
         read_data = []
-        rd = []
         with open(filename) as f:
             reader = csv.reader(f, delimiter=";")
             data_row = next(reader)
                     
             for row in reader:
-                csv_row = float(row[dati])
-                rd.append(csv_row)
-                read_data = tuple(rd)
+                csv_row = float(row[chartOpt['datInd']])
+                read_data.append(csv_row)
+                
         f.close()
         
         numbers = []
-        i = 0
         temp = []
-        #dodanie etykiet
-        for i in range(len(rd)):
+        #tworzenie domyślnych etykiet danych na osi X
+        for i in range(len(read_data)):
             numbers.append(i+1)
             temp.append(str(numbers[i]))
             labels = (tuple(temp))
 
         #tworzenie wykresu
-        if chai == 0:
+        if chartOpt['typeInd'] == 0:
             plt.bar(labels, read_data)
-        if chai == 1:
+        if chartOpt['typeInd'] == 1:
             plt.plot(labels, read_data)
 
-        plt.title(title) 
-        plt.xlabel(osx)
-        plt.ylabel(osy)              
+        plt.title(chartOpt['title']) 
+        plt.xlabel(chartOpt['labelx'])
+        plt.ylabel(chartOpt['labely'])              
         plt.show()
             
 root = Tk()
 root.title("Generator wykresów")
-root.geometry('360x100')
+root.geometry('240x100')
 
-l1 = Label(root, text = "Podaj nazwę pliku", pady=8)
-l1.grid(row=0,column=0)
+l1 = Label(root, text = "Podaj nazwę pliku").grid(row=0,column=0)
 
-e1= Entry(root, bd=3, width=25)
+e1= Entry(root, bd=3)
 e1.grid(row=1,column=0)
-
-b = Button(root, text="OK", command=read_file)
-b.grid(row=1,column=1)
-
-le = Label(root, text = "", pady=6)
+confirm = Button(root, text="OK", command=read_file).grid(row=1,column=1)
+le = Label(root, text = "")
 le.grid(row=2,column=0)
 
 root.mainloop()
