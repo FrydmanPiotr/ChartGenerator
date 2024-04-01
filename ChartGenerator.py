c4bd5c9eb0821192e2c02ffb19d7569062dca614
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2024-04-01 18:33:08 +0200

    Add files via upload
    Code refactoring

--- ChartGenerator.py
+++ ChartGenerator.py
@@ -1,6 +1,6 @@
 """
-Generator wykresów i diagramów
-Autor: Piotr Frydman
+Project: Chart and diagram generator
+Author: Piotr Frydman
 """
 import tkinter as tk
 from tkinter import Toplevel, messagebox
@@ -12,27 +12,18 @@ import os
 class ChartGenerator(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Generator wykresów")
-        self.geometry("300x100+500+250")
+        self.title("Chart generator")
+        self.geometry("250x100+300+250")
         self.resizable(False, False)
-        
-        #położenie elementów w oknie
-        self.columnconfigure(0, weight=3)
-        self.columnconfigure(1, weight=1)
-        self.columnconfigure(2, weight=2)
-        self.rowconfigure(0, weight=3)
-        self.rowconfigure(1, weight=3)
         self.create_widgets()
 
     def create_widgets(self):
-        self.name = tk.Label(self, text="Podaj nazwę pliku")
-        self.name.grid(row=0, column=0)
-        self.filename = tk.Entry(self, bd=2)
-        self.filename.grid(row=1, column=0)
-        self.search = tk.Button(self, text="Szukaj", command=self.search_file)
-        self.search.grid(row=1, column=2)
-        self.extension = tk.Label(self, text=".csv")
-        self.extension.grid(row=1, column=1)
+        tk.Label(self, text="Set filename").place(x=25,y=25)
+        self.filename=tk.Entry(self, bd=2)
+        self.filename.place(x=25,y=55)
+        tk.Button(self, text="Search",
+                  command=self.search_file).place(x=190,y=50)
+        tk.Label(self, text=".csv").place(x=152,y=55)
     
     def search_file(self):
         self.file = self.filename.get() + ".csv"
@@ -47,36 +38,26 @@ class ChartGenerator(tk.Tk):
                     file_found = True
                 else:
                     self.filename.delete(0,'end')
-                    
+                  
         if not file_found:
-            messagebox.showerror("Błąd", "Plik nie istnieje")
+            messagebox.showerror("Error", "File not found")
                 
     def read_file(self, filename):
+        #read headers to options
         with open(filename) as file:
             reader = csv.reader(file, delimiter=";")
-            columns_names = next(reader)
+            headers = next(reader)
         file.close()
+        self.configure_chart(headers, filename)
 
-        #przekazuje nazwy kolumn i pliku
-        self.create_chart(columns_names, filename)
-
-    def create_chart(self, columns_names, filename):
+    def configure_chart(self, headers, filename):
         top = Toplevel()
-        top.geometry("400x200")
-        top.title("Tworzenie wykresu")
+        top.geometry("280x250+560+200")
+        top.title("Configuring chart")
         top.grab_set()
-        top.focus()
+        top.focus()    
         
-        top.columnconfigure(0, weight=3)
-        top.columnconfigure(1, weight=3)
-        top.columnconfigure(2, weight=2)
-
-        top.rowconfigure(0, weight=2)
-        top.rowconfigure(1, weight=2)          
-       
-        chtype = ["kolumnowy", "liniowy"]
-
-        def configure_chart():
+        def create_chart():
             #przechowuje dane pobrane od użytkownika
             options = {
                 'data': data.get(),
@@ -88,57 +69,53 @@ class ChartGenerator(tk.Tk):
                 'typeInd': 0
             }
             
-            if options['data'] not in columns_names or options['type'] not in chtype:
-                    messagebox.showerror("Błąd", "Wybierz opcje z list")
+            if options['data'] not in headers or options['type'] not in chtype:
+                    messagebox.showinfo("Info", "Select options from lists")
 
             else:
-                options['datInd'] = columns_names.index(data.get())
+                options['datInd'] = headers.index(data.get())
                 options['typeInd'] = chtype.index(chart_type.get())
                 self.draw_graph(options, filename)
 
-        ldata = tk.Label(top, text="Typ danych").grid(row=0, column=0)
-        data = ttk.Combobox(top, values=columns_names[1:], state="readonly")
-        data.grid(row=0, column=1)
-        data.set("Wybierz typ danych")
+        tk.Label(top, text="Data type").place(x=20,y=10)
+        data = ttk.Combobox(top, values=headers[1:], state="readonly")
+        data.place(x=100, y=10)
+        data.set("Select data type")
 
-        lchart = tk.Label(top, text="Typ wykresu").grid(row=1, column=0)
+        chtype = ["column", "line"]
+        tk.Label(top, text="Chart type").place(x=20,y=40)
         chart_type = ttk.Combobox(top, values=chtype, state="readonly")
-        chart_type.grid(row=1, column=1)
-        chart_type.set("Wybierz typ wykresu")
+        chart_type.place(x=100,y=40)
+        chart_type.set("Select chart type")
 
-        #tytuł wykresu
-        ltitle = tk.Label(top, text="Tytuł wykresu").grid(row=3, column=0)
-        title = tk.Entry(top, bd=3, width=25)
-        title.grid(row=3, column=1)
+        tk.Label(top, text="Chart title").place(x=20,y=80)
+        title=tk.Entry(top, bd=3, width=25)
+        title.place(x=100,y=80)
 
-        #oznaczenie osi X
-        loX = tk.Label(top, text="Nazwa osi X").grid(row=4, column=0)
-        osX = tk.Entry(top, bd=3, width=25)
-        osX.grid(row=4, column=1)
+        tk.Label(top, text="X axis label").place(x=20,y=120)
+        osX=tk.Entry(top, bd=3, width=25)
+        osX.place(x=100,y=120)
 
-        #oznaczenie osi Y
-        loY = tk.Label(top, text="Nazwa osi Y").grid(row=5, column=0)
-        osY = tk.Entry(top, bd=3, width=25)
-        osY.grid(row=5, column=1)
+        tk.Label(top, text="Y axis label").place(x=20,y=160)
+        osY=tk.Entry(top, bd=3, width=25)
+        osY.place(x=100,y=160)
 
-        b = tk.Button(top, text="Utwórz", command=configure_chart)
-        b.grid(row=7, column=3)
+        tk.Button(top, text="Create",width=20,bd=4,bg="#FFE5CC",
+                  command=create_chart).place(x=60,y=200)
 
     def draw_graph(self, options, filename):
-        read_data = []
-        labels = []
+        read_data, labels = [],[]
         with open(filename) as file:
             reader = csv.reader(file, delimiter=";")
             data_row = next(reader)
                 
-            #pobranie danych
+            #read data from file
             for row in reader:
                 csv_row = row[options['datInd']]
                 read_data.append(float(csv_row))
                 names = row[0]
                 labels.append(names)
 
-            #tworzenie wykresu
             if options['typeInd'] == 0:
                 fig, ax = plt.subplots(figsize=(6, 6))
                 ax.bar(labels, read_data)
